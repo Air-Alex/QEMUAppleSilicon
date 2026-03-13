dde12f7274ec557f32724cc87b7836371a2bfd3e
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2026-03-13 12:07:38 +0200

    refactor: remove gtest remnants

--- include/glib-compat.h
+++ include/glib-compat.h
@@ -112,23 +112,6 @@ static inline gpointer g_memdup2_qemu(gconstpointer mem, gsize byte_size)
 }
 #define g_memdup2(m, s) g_memdup2_qemu(m, s)
 
-static inline bool
-qemu_g_test_slow(void)
-{
-    static int cached = -1;
-    if (cached == -1) {
-        cached = g_test_slow() || getenv("G_TEST_SLOW") != NULL;
-    }
-    return cached;
-}
-
-#undef g_test_slow
-#undef g_test_thorough
-#undef g_test_quick
-#define g_test_slow() qemu_g_test_slow()
-#define g_test_thorough() qemu_g_test_slow()
-#define g_test_quick() (!qemu_g_test_slow())
-
 #pragma GCC diagnostic pop
 
 #ifndef G_NORETURN
